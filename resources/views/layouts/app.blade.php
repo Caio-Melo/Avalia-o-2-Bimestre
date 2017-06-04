94a0ab447a5ab6b81e0ca2c76cc7356c39da84e9
Lucas Manera <caiogmelo@gmail.com>
2017-06-04 16:19:54 -0300

    autentificação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,24 +15,13 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                
+ <div class="navbar-header">
+      <a class="navbar-brand" href="/"><font size="6" color="#FF0000" face="Georgia">Revolution Filmes</font></a>
+    </div>
+               
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
@@ -43,8 +32,8 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}"><font color="#FF0000">Login</font></a></li>
+                            <li><a href="{{ route('register') }}"><font color="#FF0000">Register</font></a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,19 +12,38 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/">Revolution Filmes</a>
+      <a class="navbar-brand" href="/"><font size="6" color="#FF0000" face="Georgia">Revolution Filmes</font></a>
     </div>
     <ul class="nav navbar-nav">
-
-      <li><a href="/filmes">Filmes</a></li>
-      <li><a href="/generos">Generos</a></li>
-      <li><a href="/listas">Playlists</a></li>
-
+      <li><a href="/filmes"><font size="4" color="#FF0000">Filmes</font></a></li>
+      <li><a href="/generos"><font size="4"  color="#FF0000">Generos</font></a></li>
+      <li><a href="/listas"><font size="4"  color="#FF0000">Playlists</font></a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span><font color="#FF0000"> Login</font></a></li>
+                            <li><a href="{{ route('register') }}"><font color="#FF0000">Register</font></a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();"><font color="#FF0000">
+                                            Logout </font>
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
   </div>
 </nav>
 @yield('conteudo')
